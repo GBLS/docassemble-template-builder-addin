ac913449d68d60334f20d3c010df20f53ecd27e8
Jonathan Pyle <jhpyle@gmail.com>
2018-05-23 12:16:55 -0400

    included OfficeHelpers

--- src/js/index.js
+++ src/js/index.js
@@ -3,7 +3,7 @@
  * See LICENSE in the project root for license information.
  */
 
-//import * as OfficeHelpers from '@microsoft/office-js-helpers';
+import * as OfficeHelpers from '@microsoft/office-js-helpers';
 
 var serverName = '';
 var theVars;
