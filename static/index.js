4b287ce01d1c239063b851627c048cf0a0db588d
Jonathan Pyle <jhpyle@gmail.com>
2018-05-23 09:22:30 -0400

    added back async

--- static/index.js
+++ static/index.js
@@ -135,54 +135,53 @@ for (var i = 0; i < CommandButtonElements.length; i++) {
   new fabric['CommandButton'](CommandButtonElements[i]);
 }
 
-function testTest(){
-  return;
+////////////////////////////////////////////////////////////////
+// Docassemble code actions
+async function insertVariable() {
+    return Word.run(async context => {
+        const range = context.document.getSelection();
+
+        var variableName = document.getElementById('inputVariableName').value;
+        //checkboxVariableReplaceAll
+        var variableReplaceAll = document.getElementById('checkboxVariableReplaceAll').checked;
+        var variableFormat = document.getElementById('selectVariableFormat').value;
+
+
+        if (variableFormat == "") {
+            var textToInsert = variableName;
+        } else {
+            var textToInsert = variableFormat + '(' + variableName + ')';
+        }
+
+        range.load('text');
+
+        if (! variableReplaceAll) {
+            range.insertText('{{ ' + variableName + ' }}','Replace');
+        } else {
+            await context.sync();
+            var textToReplace = range.text;
+
+            // FIXME: We need to ignore Jinja statements and expressions -- search looks inside them now
+            var results = context.document.body.search(textToReplace.trim(), {matchWholeWord: true}); // Word Online seems to select spaces next to a word you double-click on
+            context.load(results);
+            
+            await context.sync();
+
+            for (var i = 0; i < results.items.length; i++) {
+                results.items[i].insertText('{{ ' + textToInsert + ' }}', "Replace");
+            }
+        }
+
+        await context.sync();
+    });
 }
 
-function subInsertVariable(context){
-    const range = context.document.getSelection();
-
-    var variableName = document.getElementById('inputVariableName').value;
-    var variableReplaceAll = document.getElementById('checkboxVariableReplaceAll').checked;
-    var variableFormat = document.getElementById('selectVariableFormat').value;
-
-
-    if (variableFormat == "") {
-	var textToInsert = variableName;
-    } else {
-	var textToInsert = variableFormat + '(' + variableName + ')';
-    }
-
-    range.load('text');
-
-    if (! variableReplaceAll) {
-	range.insertText('{{ ' + variableName + ' }}','Replace');
-    } else {
-	await context.sync();
-	var textToReplace = range.text;
-
-	var results = context.document.body.search(textToReplace.trim(), {matchWholeWord: true});
-	context.load(results);
-
-	await context.sync();
-
-	for (var i = 0; i < results.items.length; i++) {
-	    results.items[i].insertText('{{ ' + textToInsert + ' }}', "Replace");
-	}
-    }
-
-    await context.sync();
-}
-
-function insertVariable(){
-  return Word.run(subInsertVariable);
-}
-
-function ifPara() {
-  return Word.run(function(context){
+async function ifPara() {
+    return Word.run(async context => {
             const range = context.document.getSelection();
             var ifExpression = document.getElementById('inputIfExpression').value;
 
+            // Read the range text
             range.load('text');
             var textBefore = '{%p if ' + ifExpression + ' %}';
 
@@ -194,12 +193,13 @@ function ifPara() {
         });
 }
 
-function ifInline() {
-  return Word.run(function(context){
+async function ifInline() {
+    return Word.run(async context => {
             const range = context.document.getSelection();
             var ifExpression = document.getElementById('inputIfExpression').value;
             var textBefore = '{% if ' + ifExpression + ' %}';
 
+            // Read the range text
             range.load('text');
 
             range.insertText(textBefore,'Before');
@@ -210,8 +210,8 @@ function ifInline() {
         });
 }
 
-function listPara() {
-  return Word.run(function(context){
+async function listPara() {
+    return Word.run(async context => {
             const range = context.document.getSelection();
             var listVariableName = document.getElementById('inputListVariableName').value;
             var onlyTrue = document.getElementById('checkboxOnlyTrue').checked;
@@ -220,6 +220,7 @@ function listPara() {
             } else {
                 var textBefore = '{%p for item in ' + listVariableName + '%}'; 
             }
+            // Read the range text
             range.load('text');
             range.insertText('{{ item }}','Replace');
             range.insertParagraph(textBefore,'Before');
@@ -230,30 +231,36 @@ function listPara() {
         });
 }
 
-function commentPara() {
-  return Word.run(function(context){
+async function commentPara() {
+    return Word.run(async context => {
         const range = context.document.getSelection();
             
+        // Read the range text
         range.load('text');
-        await context.sync();
+        await context.sync(); // Guess this has a performance penalty?
         
+        // Regexp with 3 groups: {# , text between comments, #}. We match both whitespace and non-whitespace, including newlines
         var re = new RegExp('({#)([\\s\\S]*)(#})');
         var matches = re.exec(range.text);
 
-        if (matches) {
+        if (matches) { // index 1 is the uncommented string
+            // This is not correct as it removes formatting from the text
+            // This sample looks like it shows how to do it correctly: https://github.com/OfficeDev/Word-Add-in-JS-SpecKit/blob/master/scripts/boilerplate.js in addBoilerplateParagraph
+            // we should use var paragraphs = context.document.getSelection().paragraphs; and then loop through paragraph collection
             range.insertText(matches[2],'Replace'); 
             console.log('Removed comments.')
         } else {
             range.insertParagraph('{#','Before');
             range.insertParagraph('#}','After');
             console.log('Added comments.')
+            // we should extend the selection to include the newly added text
         }
         await context.sync();
     });
 }
 
-function insertTemplate() {
-  return Word.run(function(context){
+async function insertTemplate() {
+    return Word.run(async context => {
         const range = context.document.getSelection();
         var templateName = document.getElementById('inputTemplateName').value;
  
@@ -264,6 +271,7 @@ function insertTemplate() {
             var textBefore = '{{p include_docx_template("' + templateName + '", ' + templateOptions + ') }}'; 
         }
 
+        // Read the range text
         range.load('text');
         
         range.insertText(textBefore,'Replace');
@@ -273,15 +281,22 @@ function insertTemplate() {
     });
 }
 
+/////////////////////////////////////////////////////////////////////
+// Helper functions
+
+// File handling
 function getDocumentAsCompressed() {
     Office.context.document.getFileAsync(Office.FileType.Compressed, {  }, 
         function (result) {
             if (result.status == "succeeded") {
+            // If the getFileAsync call succeeded, then
+            // result.value will return a valid File Object.
             var myFile = result.value;
             var sliceCount = myFile.sliceCount;
             var slicesReceived = 0, gotAllSlices = true, docdataSlices = [];
             app.showNotification("File size:" + myFile.size + " #Slices: " + sliceCount);
 
+            // Get the file slices.
             getSliceAsync(myFile, 0, sliceCount, gotAllSlices, docdataSlices, slicesReceived);
             }
             else {
@@ -293,11 +308,16 @@ function getDocumentAsCompressed() {
 function getSliceAsync(file, nextSlice, sliceCount, gotAllSlices, docdataSlices, slicesReceived) {
     file.getSliceAsync(nextSlice, function (sliceResult) {
         if (sliceResult.status == "succeeded") {
-            if (!gotAllSlices) {
+            if (!gotAllSlices) { // Failed to get all slices, no need to continue.
                 return;
             }
+
+            // Got one slice, store it in a temporary array.
+            // (Or you can do something else, such as
+            // send it to a third-party server.)
             docdataSlices[sliceResult.value.index] = sliceResult.value.data;
             if (++slicesReceived == sliceCount) {
+               // All slices have been received.
                file.closeAsync();
                onGotAllSlices(docdataSlices);
             }
@@ -308,7 +328,7 @@ function getSliceAsync(file, nextSlice, sliceCount, gotAllSlices, docdataSlices,
             else {
                 gotAllSlices = false;
                 file.closeAsync();
-                app.showNotification("getSliceError:", sliceResult.error.message);
+                app.showNotification("getSliceAsync Error:", sliceResult.error.message);
             }
     });
 }
@@ -323,4 +343,7 @@ function onGotAllSlices(docdataSlices) {
     for (var j = 0; j < docdata.length; j++) {
         fileContent += String.fromCharCode(docdata[j]);
     }
+
+    // Now all the file content is stored in 'fileContent' variable,
+    // you can do something with it, such as print, fax...
 }
